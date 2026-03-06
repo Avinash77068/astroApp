d3dbb04200c1d6e79230204d4eb5ab017995a653
Avinash Shrivastav <avinash.shrivastav@revv.co.in>
2026-03-06 19:33:30 +0530

    code commited

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,4 @@
 export const ENV = {
-  API_BASE_URL: 'http://10.0.2.2:3000',
+  // API_BASE_URL: 'http://10.0.2.2:3000',
+  API_BASE_URL: 'https://corroborative-kneadingly-norma.ngrok-free.dev',
 };
